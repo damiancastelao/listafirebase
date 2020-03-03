ca5094936550daa51bd66112eb68a54ec33ac7a3
Damian Nogueiras <damiannogueiras@gmail.com>
2020-03-03 19:39:10 +0100

    Configurado firebase via cli

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDs5q1z_htart5akdl6DJkof_Ll-r1L9t4',
+    authDomain: 'lista-dam-cli.firebaseapp.com',
+    databaseURL: 'https://lista-dam-cli.firebaseio.com',
+    projectId: 'lista-dam-cli',
+    storageBucket: 'lista-dam-cli.appspot.com',
+    messagingSenderId: '561269825263',
+    appId: '1:561269825263:web:df7dbca0b279d390bfc51a'
+
+  }
 };
 
 /*
