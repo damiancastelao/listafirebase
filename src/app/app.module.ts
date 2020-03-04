1310dbfc744562e933a177349b3c3eaf0fe46b00
Damian Nogueiras <damiannogueiras@gmail.com>
2020-03-04 19:16:22 +0100

    Importamos el módulo de base de datos, también en nuestro servicio de autenticación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {AngularFireModule} from '@angular/fire';
 import {environment} from '../environments/environment';
+import {AngularFireModule} from '@angular/fire';
 import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
 import {RouterModule} from '@angular/router';
+
 
 @NgModule({
   declarations: [
@@ -15,6 +17,7 @@ import {RouterModule} from '@angular/router';
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireAuthModule,
+    AngularFireDatabaseModule,
     RouterModule
   ],
   providers: [],
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth} from '@angular/fire/auth';
 import { map } from 'rxjs/operators';
+import {AngularFireDatabase} from '@angular/fire/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public  auth: AngularFireAuth) { }
+  constructor(public  auth: AngularFireAuth,
+              private db: AngularFireDatabase) { }
 
   user = this.auth.authState.pipe( map( authState => {
     console.log('authState', authState);
